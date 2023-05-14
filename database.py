568d81dfb2264532c7a4f0acfa87a9cdb9d018eb
Scott Morgan <scottolivermorgan@yahoo.com>
2023-05-14 12:46:28 +0100

    created database connection.

---
+++ database.py
@@ -0,0 +1,14 @@
+from dotenv import dotenv_values
+from sqlalchemy import create_engine
+
+# Load enviroment variables to build connection string from credentials
+config = dotenv_values(".env.development")
+connection_string = f"mysql+{config['DRIVERNAME']}://\
+                            {config['USERNAME']}:\
+                            {config['PASSWORD']}@\
+                            {config['SERVER']}:\
+                            {config['PORT']}/\
+                            {config['DBNAME']}"
+
+# Connect to MySQL database
+engine = create_engine(connection_string, echo=True)
