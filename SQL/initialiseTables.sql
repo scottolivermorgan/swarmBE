1bed0f4303f72e0de18c06144156c0a5c72fea2e
Scott Morgan <scottolivermorgan@yahoo.com>
2023-05-14 11:32:37 +0100

    changed to MySQL syntax, expanded table schema.

--- SQL/initialiseTables.sql
+++ SQL/initialiseTables.sql
@@ -1,16 +1,15 @@
--- CREATE DATABASE swarm_labs
-
+-- CREATE DATABASE swarm_labs;
 USE swarm_labs;
 
--- Drop all tables if they exist
-
-IF OBJECT_ID('[dbo].[Employee]','U') IS NOT NULL
-DROP TABLE [dbo].[Employee]
-GO
-
 -- Create the table in the specified schema
-CREATE TABLE [dbo].[Employee]
-(
-    [id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY
-    [name] VARCHAR(320) NOT NULL,
-)
+DROP TABLE IF EXISTS Employee;
+CREATE TABLE Employee(  
+    id INT NOT NULL AUTO_INCREMENT,  
+    name VARCHAR(320) NOT NULL,
+    role VARCHAR(320) NOT NULL,
+    img VARCHAR(320) NOT NULL,
+    titles VARCHAR(320) NOT NULL,
+    bio VARCHAR(320) NOT NULL,
+    links VARCHAR(320) NOT NULL,
+    PRIMARY KEY(id)
+);
--- SQL/populateTables.sql
+++ SQL/populateTables.sql
@@ -1,6 +1,26 @@
 USE swarm_labs;
 
-INSERT INTO [dbo].[Employee](name)
-VALUES
-('Scott Morgan')
-GO
+INSERT INTO Employee (name,role,img,titles,bio,links)
+VALUEs
+	(
+	'Scott Morgan','Lead Scientist & Engineer','/path','[PhD., MPhys., PGcert.]',
+	'Passionate surfer whose enthusiasm far exceeds his skill',
+	'[https://www.linkedin.com/in/scott-oliver-morgan/, 
+	https://scholar.google.com/citations?user=LI_qhRsAAAAJ&hl=en, 
+	https://github.com/scottolivermorgan?tab=repositories]'
+	),
+	(
+	'Chris Lowe','Consultant Scientist','/path','[MPhys.]',
+	'Avid motorbike fan, technologist & wannabe chef.',
+	'[https://www.linkedin.com/in/chris-lowe-86221899/,]'
+	),
+	(
+	'Charlie Street','Lead Sales','/path','[]',
+	'Tenacious outdoorsman, forager and spear fisherman.',
+	'[https://www.linkedin.com/in/charlie-heath-a3a435135/,]'
+	),
+    (
+	'Jacqueline Maxfield','Lead Branding and Design','/path','[BSc Hons.]',
+	'Graphic designer & cat lover with an obsession for typography.',
+	'[https://www.linkedin.com/in/maxfieldjacqueline/,]'
+	);
