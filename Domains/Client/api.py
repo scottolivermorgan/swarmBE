39a7cf291973526751d20707d549d85d4d5e366b
Scott Morgan <scottolivermorgan@yahoo.com>
2023-05-27 17:08:06 +0100

    dsf

--- Domains/Client/api.py
+++ Domains/Client/api.py
@@ -1,9 +1,17 @@
 from flask import Blueprint, request,jsonify,abort
 from flask_api import status
-from sqlalchemy import func, select
+from sqlalchemy import func, select,text
+from database import engine
+
 
 clients_bp = Blueprint("clients_blueprint", __name__)
 
 @clients_bp.route('/',methods = ['GET'])
 def home():
     return "hello worlds"
+
+@clients_bp.route('/employee_list',methods = ['GET'])
+def empoloyee_list():
+    with engine.connect() as connection:
+        var = connection.execute(text('select * from Employee'))
+        return 'yay'
--- database.py
+++ database.py
@@ -3,12 +3,8 @@ from sqlalchemy import create_engine
 
 # Load enviroment variables to build connection string from credentials
 config = dotenv_values(".env.development")
-connection_string = f"mysql+{config['DRIVERNAME']}://\
-                            {config['USERNAME']}:\
-                            {config['PASSWORD']}@\
-                            {config['SERVER']}:\
-                            {config['PORT']}/\
-                            {config['DBNAME']}"
+connection_string = f"mysql+{config['DRIVERNAME']}://{config['USERNAME']}:{config['PASSWORD']}@{config['SERVER']}:{config['PORT']}/{config['DBNAME']}"
 
+print(connection_string)
 # Connect to MySQL database
 engine = create_engine(connection_string, echo=True)
